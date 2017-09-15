3373f16d1795928005ece0b7b5798c1b99c37a40
JackNoordhuis <Jack@CrazedCraftMC.net>
2017-09-16 00:53:57 +1000

    Bump API, add correct file headers and use safe_var_dump()

--- EffectEventTesting/src/effecteventtesting/EventListener.php
+++ EffectEventTesting/src/effecteventtesting/EventListener.php
@@ -1,10 +1,18 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: Jack
- * Date: 4/12/16
- * Time: 9:28 PM
+ * EffectEventTesting plugin for PocketMine-MP
+ * Copyright (C) 2017 JackNoordhuis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 namespace effecteventtesting;
@@ -31,11 +39,11 @@ class EventListener implements Listener {
 	}
 
 	public function onEffectAdd(EntityEffectAddEvent $event) {
-		var_dump($event);
+		safe_var_dump($event);
 	}
 
 	public function onEffectRemove(EntityEffectRemoveEvent $event) {
-		var_dump($event);
+		safe_var_dump($event);
 	}
 
 	/**
--- EffectEventTesting/src/effecteventtesting/Main.php
+++ EffectEventTesting/src/effecteventtesting/Main.php
@@ -1,10 +1,18 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: Jack
- * Date: 4/12/16
- * Time: 9:28 PM
+ * EffectEventTesting plugin for PocketMine-MP
+ * Copyright (C) 2017 JackNoordhuis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 namespace effecteventtesting;
@@ -13,9 +21,6 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase {
-
-	/** @var Config */
-	private $settings = null;
 
 	/** @var EventListener */
 	private $listener = null;
